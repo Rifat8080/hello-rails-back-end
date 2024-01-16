728f2d4132059a9e45ecb18d11f4107e79f45abc
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-01-16 22:18:13 +0600

    backend upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get '/random_greeting', to: 'greetings#random_greeting'
-  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get 'random_greeting', to: 'greetings#random_greeting'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
+# Examples:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-# db/seeds.rb
-
-Message.create(message: "Hello, World!")
-Message.create(message: "Greetings from Rails!")
-Message.create(message: "Welcome to the API!")
-Message.create(message: "Hi there!")
-Message.create(message: "Good day!")
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello world")
+Greeting.create(message: "Hi")
+Greeting.create(message: "Hello, this is from rails backend")
+Greeting.create(message: "Hello, Have a good day")
+Greeting.create(message: "Hello to frontend")
